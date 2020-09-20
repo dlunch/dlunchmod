bd8f13cdf9654b7b8acc77e7d7b537d8b7029274
Inseok Lee <dlunch@gmail.com>
2020-09-20 22:30:28 +0900

    Add recipe only if overhaul enabled

--- data-updates.lua
+++ data-updates.lua
@@ -13,47 +13,50 @@ angelsmods.functions.make_void("omnic-waste", "water")
 remove_tech_prerequisite("nuclear-power", "effectivity-module-2")
 
 -- add omnite to rubyte, bobomnium recipes
--- copied from omnimatter/prototypes/omniore.lua
-local initial_recipes = {}
-local inputs = {}
-initial_recipes[#initial_recipes + 1] = {
-  {
-    type = "item",
-    name = "angels-ore5",
-    amount = 1
-  },
-  {
-    type = "item",
-    name = "stone-crushed",
-    amount = 5
-  },
-}
-inputs[#inputs + 1] = 6
-initial_recipes[#initial_recipes + 1] = {
-  {
-    type = "item",
-    name = "angels-ore6",
-    amount = 1
-  },
-  {
-    type = "item",
-    name = "stone-crushed",
-    amount = 5
-  },
-}
-inputs[#inputs + 1] = 6
+-- run only if angel's industries overhaul enabled
+if angelsmods.industries.tech then
+  -- copied from omnimatter/prototypes/omniore.lua
+  local initial_recipes = {}
+  local inputs = {}
+  initial_recipes[#initial_recipes + 1] = {
+    {
+      type = "item",
+      name = "angels-ore5",
+      amount = 1
+    },
+    {
+      type = "item",
+      name = "stone-crushed",
+      amount = 5
+    },
+  }
+  inputs[#inputs + 1] = 6
+  initial_recipes[#initial_recipes + 1] = {
+    {
+      type = "item",
+      name = "angels-ore6",
+      amount = 1
+    },
+    {
+      type = "item",
+      name = "stone-crushed",
+      amount = 5
+    },
+  }
+  inputs[#inputs + 1] = 6
 
-for index, result in pairs(initial_recipes) do
-	RecGen:create("omnimatter","initial-omnitraction-" .. result[1].name):
-	setCategory("omnite-extraction-burner"):
-	setEnergy(5):
-	setEnabled():
-	noItem():
-	setSubgroup("omni-basic"):
-	setIngredients({"omnite", inputs[index]}):
-	setResults(result):
-	setIcons(result[1].name):
-	marathon():
-	setLocName("recipe-name.initial-omni","item-name."..result[1].name):
-	addSmallIcon(result[2].name, 3):extend()
+  for index, result in pairs(initial_recipes) do
+    RecGen:create("omnimatter","initial-omnitraction-" .. result[1].name):
+    setCategory("omnite-extraction-burner"):
+    setEnergy(5):
+    setEnabled():
+    noItem():
+    setSubgroup("omni-basic"):
+    setIngredients({"omnite", inputs[index]}):
+    setResults(result):
+    setIcons(result[1].name):
+    marathon():
+    setLocName("recipe-name.initial-omni","item-name."..result[1].name):
+    addSmallIcon(result[2].name, 3):extend()
+  end
 end
