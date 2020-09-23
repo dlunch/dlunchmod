3cf9c119dd6de3c204a9e8bf62be340754d34a55
Inseok Lee <dlunch@gmail.com>
2020-09-23 12:31:35 +0900

    Fix recipe ingredient update

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -2,9 +2,13 @@ function replace_recipe_ingredient(recipe_name, ingredient, to_ingredient)
   for i, item in pairs(data.raw.recipe[recipe_name].ingredients) do
     if item.name == ingredient then
       data.raw.recipe[recipe_name].ingredients[i].name = to_ingredient
+    elseif item[1] == ingredient then
+      item[1] = to_ingredient
     end
   end
 end
 
 -- omnimatter + AngelBobExtended
+log(serpent.block(data.raw.recipe["tech-component-red"]))
 replace_recipe_ingredient("tech-component-red", "coal", "omnite")
+log(serpent.block(data.raw.recipe["tech-component-red"]))
