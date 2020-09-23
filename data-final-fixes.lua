424db6c6d56ad18a0032d96aef930b4e9c291d82
Inseok Lee <dlunch@gmail.com>
2020-09-23 15:25:18 +0900

    Apply only if exists

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -9,6 +9,6 @@ function replace_recipe_ingredient(recipe_name, ingredient, to_ingredient)
 end
 
 -- omnimatter + AngelBobExtended
-log(serpent.block(data.raw.recipe["tech-component-red"]))
-replace_recipe_ingredient("tech-component-red", "coal", "omnite")
-log(serpent.block(data.raw.recipe["tech-component-red"]))
+if data.raw.recipe["tech-component-red"] then
+  replace_recipe_ingredient("tech-component-red", "coal", "omnite")
+end
