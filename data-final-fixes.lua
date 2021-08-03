02c132a1ecff35b7e559f09530dfd93e40c536e4
Inseok Lee <git@dlun.ch>
2021-08-03 18:47:25 +0900

    Yuoki + extendedangels

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,5 +1,26 @@
 require('./lib.lua')
 
+-- extendedangels + yuoki
+local recipe_list = {
+    "pellet-tungsten-smelting",
+    "tungsten-carbide",
+    "tungsten-carbide-2",
+    "copper-tungsten-alloy"
+}
+
+
+for _, recipe in pairs(recipe_list) do
+	for _, module in pairs(data.raw.module) do
+		if module.limitation then
+			for i, limit in pairs(module.limitation) do
+				if limit == recipe then
+					table.remove(module.limitation, i)
+				end
+			end
+		end
+	end
+end
+
 -- ir2 + jetpack duplicate
 if data.raw.technology["jetpack-4"] then
   remove_tech_prerequisite("jetpack-4", "ir2-modules-3")
