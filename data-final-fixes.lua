9052873bf896274cabb6e8c5d3a20990203773aa
Inseok Lee <git@dlun.ch>
2022-06-05 12:50:47 +0900

    Disable rampant arsenal power armor mk3 on krastorio2

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -50,11 +50,15 @@ if data.raw.technology["surveillance-2"] then
 	data.raw.technology["surveillance-2"].hidden = true
 	data.raw.technology["surveillance-2"].enabled = false
 end
--- disable rampant arsenal nuclear locomotive if k2 loaded
-if data.raw.locomotive["nuclear-train-vehicle-rampant-arsenal"] and data.raw.locomotive["kr-nuclear-locomotive"] then
+
+if mods["Krastorio2"] and mods["RampantArsenal"] then
+  -- disable rampant arsenal nuclear locomotive if k2 loaded
   data.raw["recipe"]["kr-vc-nuclear-train-vehicle-rampant-arsenal"] = nil
   data.raw["recipe"]["nuclear-train-vehicle-rampant-arsenal"] = nil
   data.raw["item-with-entity-data"]["nuclear-train-vehicle-rampant-arsenal"] = nil
   data.raw.locomotive["nuclear-train-vehicle-rampant-arsenal"] = nil
   data.raw.technology["rampant-arsenal-technology-nuclear-railway"] = nil
+
+  -- disable rampant arsenal power armor mk3 if k2 loaded
+  data.raw.technology["rampant-arsenal-technology-power-armor-mk3"] = nil
 end
